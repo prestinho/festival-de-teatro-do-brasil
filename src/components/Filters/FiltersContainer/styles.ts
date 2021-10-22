7860a377ea3c4f445efb593b4d696dbb11f2742a
Fernando Rybka <fernando.pr@gmail.com>
2021-10-21 22:27:56 -0300

    fix: styles corrected

--- src/components/Filters/FiltersContainer/styles.ts
+++ src/components/Filters/FiltersContainer/styles.ts
@@ -3,13 +3,18 @@
 export const Container = styled.div`
     /* background: rgba(255,255,255,0.3); */
     background-image: linear-gradient(135deg, #8c6fff, #01ff8aab); 
+
+    display: flex;
+    justify-content: space-between;
     border-radius: 10px;
     width: 80%;
     margin: auto;
     padding: 1.5rem;
     margin-bottom: 2rem;
 
-    h2 {
+    h3 {
         color: white;
+        line-height: 1.5rem;
+        align-self: center;
     }
 `;
